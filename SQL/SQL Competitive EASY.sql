31e2a0f02ebfb71e7360f004fc54a96e761aa6ba
Utkarsh Singh <75792507+UtkarshS007@users.noreply.github.com>
2025-03-01 05:28:16 -0500

    New Problems Solved

--- SQL/SQL Competitive EASY.sql
+++ SQL/SQL Competitive EASY.sql
@@ -60,5 +60,91 @@ ORDER BY
     total_workers DESC;
 
 
+-- Question 4: Find the last time each bike was in use. Output both the bike number and the date-timestamp of the 
+--             bike's last use (i.e., the date-time the bike was returned). Order the results by bikes that were most
+--             recently used.
+
+SELECT bike_number, end_time
+FROM dc_bikeshare_q1_2012
+GROUP BY bike_number
+ORDER BY end_time DESC LIMIT 1;
+
+-- Question 5: Find the details of each customer regardless of whether the customer made an order. 
+--             Output the customer's first name, last name, and the city along with the order details.
+--             Sort records based on the customer's first name and the order details in ascending order.
+
+select c.first_name, c.last_name, c.city, o.order_details
+FROM customers c 
+LEFT JOIN orders o 
+ON c.id = o.cust_id
+ORDER BY 
+    c.first_name ASC,
+    o.order_details ASC;
+
+
+-- Question 6: Find the average number of bathrooms and bedrooms for each city’s property types. 
+--             Output the result along with the city name and the property type.
+
+WITH avg_details AS (
+    SELECT 
+        AVG(bedrooms) AS AVG_BEDR, 
+        AVG(bathrooms) AS AVG_BATHR, 
+        city, 
+        property_type
+    FROM 
+        airbnb_search_details
+    GROUP BY 
+        city, property_type
+)
+SELECT * FROM avg_details;
+
+-- Question 6: Find how many reviews exist for each review score given to 'Hotel Arena'. Output the
+--             hotel name ('Hotel Arena'), each review score, and the number of reviews for that score.
+--             Ensure the results only include 'Hotel Arena.'
+
+SELECT 
+    'Hotel Arena' AS hotel_name,
+    reviewer_score AS review_score,
+    COUNT(*) AS number_of_reviews
+FROM 
+    hotel_reviews
+WHERE 
+    hotel_name = 'Hotel Arena'
+GROUP BY 
+    reviewer_score
+ORDER BY 
+    reviewer_score DESC;
+
+-- Question 7: Count the number of movies for which Abigail Breslin was nominated for an Oscar.
+
+select COUNT(movie) AS movies_nominated_by_Abigail
+FROM oscar_nominees
+WHERE nominee = 'Abigail Breslin';
+
+-- Question 8: Write a query that returns the number of unique users per client per month
+
+WITH user_counts_per_month AS (
+    SELECT 
+        client_id, 
+        COUNT(DISTINCT user_id) AS unique_users, 
+        MONTH(time_id) AS month
+    FROM 
+        fact_events
+    GROUP BY 
+        client_id, 
+        MONTH(time_id)
+)
+SELECT 
+    client_id, 
+    unique_users, 
+    month
+FROM 
+    user_counts_per_month
+ORDER BY 
+    client_id, 
+    month;
+
+
+
 
 
